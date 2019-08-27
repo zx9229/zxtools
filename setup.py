1ff46a8bcbc07ff6ad4a5827abc11383acbe6ed0
zx <zx@zj.com>
2019-08-27 23:41:46 +0800

    2019-8-27 23:41:42 临时提交

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
-setup(
-    name="zxt",
-    version="0.0.20190827",
-    packages=find_packages(),
-)
 
-# name参数的值, 就是 pip list 显示的名字, 但不是 import 的名字.
+setup(name="zxt", version="0.0.20190827.0", packages=find_packages())
+
+# name参数的值, 就是 pip list 显示的名字, 但(可以)不是 import 的名字.
 # import 的名字, 是(最顶层的 __init__.py 所在的目录)的名字.
 
 # 自定义包
