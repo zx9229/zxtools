eba9620e9ea917ab22530fe03d1e853abc5ddcaf
zx <zx@zj.com>
2019-11-26 18:21:12 +0800

    2019-11-26 18:21:08 临时提交

--- zxt/temp/math.py
+++ zxt/temp/math.py
@@ -9,8 +9,8 @@ def adbf(f: float, sign: bool = False, aD: int = 0, bF: int = 6):
     print(adbf(3.14,  True, 4, 6)) => [  +3.140000]
     '''
     srcStr = ('%' + ('+' if sign else '') + '.*f') % (bF, f)
-    oldStr = ('%' + ('+' if sign else '') + 'd') % (f)
-    newStr = ('%' + ('+' if sign else '') + '*d') % (aD, f)
+    oldStr = ('%' + ('+' if sign else '') + 'd.') % (f)
+    newStr = ('%' + ('+' if sign else '') + '*d.') % (aD, f)
     dstStr = srcStr.replace(oldStr, newStr)
     return dstStr
 
