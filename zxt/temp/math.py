311c41cc2e52b9e7ff57e44273328d71d8648c96
zx <zx@zj.com>
2019-11-26 18:08:48 +0800

    2019-11-26 18:08:44 临时提交

--- zxt/temp/math.py
+++ zxt/temp/math.py
@@ -1,22 +1,18 @@
 import math
 
 
-def adbf(f: float, symbol: bool = False, aD: int = 0, bF: int = 6):
+def adbf(f: float, sign: bool = False, aD: int = 0, bF: int = 6):
     '''
     print(adbf(3.14, False, 0, 6)) => [3.140000]
     print(adbf(3.14, False, 4, 6)) => [   3.140000]
     print(adbf(3.14,  True, 0, 6)) => [+3.140000]
     print(adbf(3.14,  True, 4, 6)) => [  +3.140000]
     '''
-    symbolStr = '+' if symbol else ''
-    fmtF = '%{0:s}{1:d}.{2:d}f'.format(symbolStr, aD, bF)
-    fm1D = '%{0:s}{1:d}d.'.format(symbolStr, aD)
-    fm2D = '%{0:s}d.'.format(symbolStr, aD)
-    fmtFs = fmtF % f
-    fm1Ds = fm1D % f
-    fm2Ds = fm2D % f
-    sssss = fmtFs.replace(fm2Ds, fm1Ds)
-    return sssss
+    srcStr = ('%' + ('+' if sign else '') + '.*f') % (bF, f)
+    oldStr = ('%' + ('+' if sign else '') + 'd') % (f)
+    newStr = ('%' + ('+' if sign else '') + '*d') % (aD, f)
+    dstStr = srcStr.replace(oldStr, newStr)
+    return dstStr
 
 
 if __name__ == '__main__':
